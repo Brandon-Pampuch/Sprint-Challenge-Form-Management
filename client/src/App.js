8f59d60fa3d68ea9cce2d2db76b66971a8feffef
Brandon_Pampuch <brandonpampuch@gmail.com>
2019-09-04 11:52:08 -0500

    add basic file structure

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+class App extends Component {
+  state = { 
+    response: ''
+   }
+
+
+   
+  render() { 
+    return ( 
+    <div>
+      <h1>hello</h1>
+    </div> 
+    );
+  }
+}
+ 
+export default App;
+
+
---
+++ client/src/components/Form.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Form extends Component {
+    state = { 
+        text: ''
+     }
+    render() { 
+        return ( <div>
+            <form>
+                <input>
+                </input>
+            </form>
+        </div> );
+    }
+}
+ 
+export default Form;
